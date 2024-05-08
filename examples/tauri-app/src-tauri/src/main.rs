5bfde9d3dbf496ffd929c2c52c747fd3873a6d25
clearlysid <siddharth99c@gmail.com>
2024-05-08 11:18:44 +0000

    feat: adds pseudo-code for approach

--- examples/tauri-app/src-tauri/src/main.rs
+++ examples/tauri-app/src-tauri/src/main.rs
@@ -1,6 +1,77 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
+fn emulate_win_z() -> Result<(), anyhow::Error> {
+    use windows::Win32::UI::Input::KeyboardAndMouse::{
+        SendInput, INPUT, INPUT_KEYBOARD, KEYBD_EVENT_FLAGS, KEYEVENTF_KEYUP, VIRTUAL_KEY, VK_RWIN,
+    };
+
+    unsafe {
+        let mut inputs: [INPUT; 4] = std::mem::zeroed();
+        inputs[0].r#type = INPUT_KEYBOARD;
+        inputs[0].Anonymous.ki.wVk = VK_RWIN;
+        inputs[0].Anonymous.ki.dwFlags = KEYBD_EVENT_FLAGS(0);
+
+        inputs[1].r#type = INPUT_KEYBOARD;
+        inputs[1].Anonymous.ki.wVk = VIRTUAL_KEY('Z' as u16);
+        inputs[1].Anonymous.ki.dwFlags = KEYBD_EVENT_FLAGS(0);
+
+        inputs[2].r#type = INPUT_KEYBOARD;
+        inputs[2].Anonymous.ki.wVk = VIRTUAL_KEY('Z' as u16);
+        inputs[2].Anonymous.ki.dwFlags = KEYEVENTF_KEYUP;
+
+        inputs[3].r#type = INPUT_KEYBOARD;
+        inputs[3].Anonymous.ki.wVk = VK_RWIN;
+        inputs[3].Anonymous.ki.dwFlags = KEYEVENTF_KEYUP;
+
+        SendInput(&inputs, std::mem::size_of::<INPUT>() as _);
+    }
+
+    // wait 100 ms
+    std::thread::sleep(std::time::Duration::from_millis(100));
+
+    unsafe {
+        let mut inputs: [INPUT; 2] = std::mem::zeroed();
+        inputs[0].r#type = INPUT_KEYBOARD;
+        inputs[0].Anonymous.ki.wVk = VIRTUAL_KEY(0x12); // VK_MENU (Alt key)
+        inputs[0].Anonymous.ki.dwFlags = KEYBD_EVENT_FLAGS(0);
+
+        inputs[1].r#type = INPUT_KEYBOARD;
+        inputs[1].Anonymous.ki.wVk = VIRTUAL_KEY(0x12); // VK_MENU (Alt key)
+        inputs[1].Anonymous.ki.dwFlags = KEYEVENTF_KEYUP;
+
+        SendInput(&inputs, std::mem::size_of::<INPUT>() as _);
+    }
+
+    Ok(())
+}
+
+#[tauri::command]
+async fn show_snap_overlay() -> Result<(), String> {
+    emulate_win_z().map_err(|e| e.to_string())
+}
+
 fn main() {
-  tauri_app_lib::run();
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![show_snap_overlay])
+        .plugin(tauri_plugin_decorum::init())
+        .setup(|app| {
+            #[cfg(target_os = "windows")]
+            {
+                let window = app.get_webview_window("main").unwrap();
+
+                use tauri_plugin_decorum::WebviewWindowExt;
+
+                window.set_decorations2();
+
+                // window.set_shadow(true).expect("couldn't set shadow");
+
+                // window_vibrancy::apply_mica(window, Some(true)).expect("couldn't set mica");
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,50 @@ mod commands;
 use desktop::Decorum;
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the decorum APIs.
-pub trait DecorumExt<R: Runtime> {
+pub trait WebviewWindowExt<R: Runtime> {
     fn transparent_titlebar(&self) -> &Decorum<R>;
+    fn set_native_titlebar(&self) -> Result<(), ()>;
 }
 
-impl<R: Runtime, T: Manager<R>> crate::DecorumExt<R> for T {
+impl<R: Runtime, T: Manager<R>> crate::WebviewWindowExt<R> for T {
     fn transparent_titlebar(&self) -> &Decorum<R> {
+        println!("transparent_titlebar called");
         self.state::<Decorum<R>>().inner()
     }
+    fn set_native_titlebar(&self) -> Result<(), ()> {
+        // Windows
+
+        // TODO: self is Window type
+
+        // Get webview of this window
+        // Run JS
+        // JS:
+        // 1. check for presence of titlebar-elements via data
+        // 2. if found, attach events
+        // 3. if not found, create custom elements and attach events
+
+        // Mac: set titlebar to overlay
+        // #[cfg(target_os = "macos")]
+        // {
+        //     editor_win = editor_win
+        //         .title_bar_style(tauri::TitleBarStyle::Overlay)
+        //         .hidden_title(true);
+        // }
+
+        Ok(())
+    }
 }
+
+// pub trait WebviewWindowExt {
+//     fn set_decorations(&self, decorations: bool) -> Result<(), ()>;
+// }
+
+// impl WebviewWindowExt for tauri::WebviewWindow {
+//     fn set_decorations(&self, decorations: bool) -> Result<(), ()> {
+//         println!("set_decorations called");
+//         Ok(())
+//     }
+// }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
