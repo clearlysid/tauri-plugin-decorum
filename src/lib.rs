407e5244788302cab3e4a504921d2981d3596404
amitchaudhari9121 <amitchaudhari9121@gmail.com>
2024-05-11 01:28:30 +0530

    fix: cfg targer win macos differentiation added

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,17 @@ use tauri::WebviewWindow;
 mod commands;
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the decorum APIs.
-
+#[cfg(target_os = "windows")]
 pub trait WebviewWindowExt {
     fn create_overlay_titlebar(self) -> Result<WebviewWindow, Error>;
+}
+
+#[cfg(not(target_os = "windows"))]
+pub trait WebviewWindowExt {
     fn set_traffic_light_inset(self, x: f32, y: f32) -> Result<WebviewWindow, Error>;
 }
 
+#[cfg(target_os = "windows")]
 impl<'a> WebviewWindowExt for WebviewWindow {
     fn create_overlay_titlebar(self) -> Result<WebviewWindow, Error> {
         self.set_decorations(false)
@@ -29,6 +34,11 @@ impl<'a> WebviewWindowExt for WebviewWindow {
 
         Ok(self)
     }
+
+}
+
+#[cfg(not(target_os = "windows"))]
+impl<'a> WebviewWindowExt for WebviewWindow {
     fn set_traffic_light_inset(self, x: f32, y: f32) -> Result<WebviewWindow, Error> {
         let ns_window = self.ns_window().expect("couldn't get ns_window");
 
