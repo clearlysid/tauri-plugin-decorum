9becb2392e95947a478fc241600343bf1d87c241
clearlysid <siddharth99c@gmail.com>
2024-05-12 09:19:51 +0530

    feat: pass window by ref

--- src/lib.rs
+++ src/lib.rs
@@ -9,32 +9,34 @@ mod commands;
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the decorum APIs.
 pub trait WebviewWindowExt {
-    fn create_overlay_titlebar(self) -> Result<WebviewWindow, Error>;
+    fn create_overlay_titlebar(&self) -> Result<&WebviewWindow, Error>;
     #[cfg(target_os = "macos")]
-    fn set_traffic_light_inset(self, x: f32, y: f32) -> Result<WebviewWindow, Error>;
+    fn set_traffic_lights_inset(&self, x: f32, y: f32) -> Result<&WebviewWindow, Error>;
 }
 
 impl<'a> WebviewWindowExt for WebviewWindow {
-    fn create_overlay_titlebar(self) -> Result<WebviewWindow, Error> {
+    fn create_overlay_titlebar(&self) -> Result<&WebviewWindow, Error> {
+        #[cfg(target_os = "windows")]
         self.set_decorations(false)
-            .expect("failed to set decorations");
+            .expect("failed to hide decorations");
 
         let win2 = self.clone();
 
         self.listen("decorum-page-load", move |_| {
-            // get the file script.js as a string
-            // The snippet checks for ab existing elment with data-tauri-decorum-tb
-            // and creates a windows "default" titlebar if not found.
             // println!("decorum-page-load event received")
+
+            // get the file script.js as a string
             let script = include_str!("script.js");
             win2.eval(script).expect("couldn't run js");
+            // The snippet checks for an existing elment with data-tauri-decorum-tb
+            // and creates a windows "default" titlebar if not found.
         });
 
         Ok(self)
     }
 
     #[cfg(target_os = "macos")]
-    fn set_traffic_light_inset(self, x: f32, y: f32) -> Result<WebviewWindow, Error> {
+    fn set_traffic_lights_inset(&self, x: f32, y: f32) -> Result<&WebviewWindow, Error> {
         let ns_window = self.ns_window().expect("couldn't get ns_window");
 
         Ok(self)
@@ -47,7 +49,7 @@ pub fn init<R: Runtime>() -> TauriPlugin<R> {
         .on_page_load(|window, _payload| {
             window
                 .emit("decorum-page-load", ())
-                .expect("couldn't fire event");
+                .expect("couldn't fire decorum-page-load event");
         })
         .build()
 }
